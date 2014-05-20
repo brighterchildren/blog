63037238e804266c700d0d2144fc715da79de027
William Reynolds <wrr3j@virginia.edu>
2014-05-19 21:48:59 -0700

    Fix logo link

--- wp-content/themes/brighter-children/header.php
+++ wp-content/themes/brighter-children/header.php
@@ -53,7 +53,7 @@
 			<div class="container">
 				<!-- Brand and toggle get grouped for better mobile display -->
 				<div class="navbar-header">
-					<a class="navbar-brand" href="#"><span class="logo">Brighter Children</span></a>
+					<a class="navbar-brand" href="/"><span class="logo">Brighter Children</span></a>
 				</div>
 
 				<!-- Collect the nav links, forms, and other content for toggling -->

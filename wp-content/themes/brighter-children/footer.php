967dc9d19d14e81ca7211ef03c97f392b2118487
William Reynolds <wrr3j@virginia.edu>
2014-05-30 22:33:23 -0700

    Add analytics snippet

--- wp-content/themes/brighter-children/footer.php
+++ wp-content/themes/brighter-children/footer.php
@@ -126,6 +126,19 @@
 			
 			// code here...
 		</script>
+
+		<!-- GA -->
+		<script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		  ga('create', 'UA-41555584-1', 'brighterchildren.org');
+		  ga('send', 'pageview');
+
+		</script>
+
 		<!-- Custom JS -->
 		<script src="wp-content/themes/brighter-children/js/custom.js"></script>
 		

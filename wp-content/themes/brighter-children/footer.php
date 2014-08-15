3db7b31e3af1ee05de4b9fc1e7dc9a60023de959
William Reynolds <wrr3j@virginia.edu>
2014-08-14 23:27:03 -0700

    Move GA code to head tag

--- wp-content/themes/brighter-children/footer.php
+++ wp-content/themes/brighter-children/footer.php
@@ -129,18 +129,6 @@
 			// code here...
 		</script>
 
-		<!-- GA -->
-		<script>
-		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-		  ga('create', 'UA-41555584-1', 'brighterchildren.org');
-		  ga('send', 'pageview');
-
-		</script>
-
 		<!-- ClickTale Bottom part -->
 		<script type='text/javascript'>		 
 		document.write(unescape("%3Cscript%20src='"+
